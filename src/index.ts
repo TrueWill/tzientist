dd250b34d787f88f34d4ea51008f96481346183b
Bill Sorensen <baldgoth@gmail.com>
2020-02-29 14:27:22 -0700

    Correct type of experiment function
    https://stackoverflow.com/questions/60469219/enforce-that-two-functions-take-the-same-parameter-types-in-typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ExperimentFunction<T> = (...args: any[]) => T;
+export type ExperimentFunction<TParams extends any[], TResult> = (
+  ...args: TParams
+) => TResult;
 
-export function experiment<T>({
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function experiment<TParams extends any[], TResult>({
   name,
   use,
   check
 }: {
   name: string;
-  use: ExperimentFunction<T>;
-  check: ExperimentFunction<T>;
-}): ExperimentFunction<T> {
-  return (...args): T => {
-    const checkResult: T = check(...args);
-    const useResult: T = use(...args);
+  use: ExperimentFunction<TParams, TResult>;
+  check: ExperimentFunction<TParams, TResult>;
+}): ExperimentFunction<TParams, TResult> {
+  return (...args): TResult => {
+    const checkResult = check(...args);
+    const useResult = use(...args);
 
     if (useResult !== checkResult) {
       console.log(`Experiment ${name}: difference found`);
